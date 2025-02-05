bcf57dc17dd02f9fd11d0e1bf6e7870ceee846df
CarlosJuarezXYZ <carlostuna015@gmail.com>
2025-02-05 14:09:52 -0500

    feat(charls): add motion in app to all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,24 @@
 import "@/styles/globals.css";
+import { motion } from "framer-motion";
 import type { AppProps } from "next/app";
 import NavBar from "../components/NavBar/NavBar";
+import { useRouter } from "next/router"; 
 import styled from "styled-components";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   return (
     <ContainerHome>
       <NavBar />
-      <Component {...pageProps} />
+      <motion.div
+      key={router.route}
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -20 }}
+        transition={{ duration: 0.5 }}
+      >
+        <Component {...pageProps} />
+      </motion.div>
     </ContainerHome>
   );
 }
